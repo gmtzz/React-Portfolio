f01bc0a0ef7018a3b35712df529d9d4a486f0d76
Giselle <martinezgiselle5@gmail.com>
2023-11-01 14:47:46 -0400

    added contact form

--- react-portfolio/src/components/contact/index.jsx
+++ react-portfolio/src/components/contact/index.jsx
@@ -1,10 +1,75 @@
-import React from 'react'
+import { useState } from "react"
 import "./contact.css"
+import emailjs from "emailjs-com"
+  const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-const Contact = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Your Email.js service ID, template ID, and user ID |go to emailjs and sign up for this to work
+    const serviceID = 'YOUR_SERVICE_ID';
+    const templateID = 'YOUR_TEMPLATE_ID';
+    const userID = 'YOUR_USER_ID';
+
+    const templateParams = {
+      from_name: name,
+      reply_to: email,
+      message: message,
+    };
+
+    emailjs.send(serviceID, templateID, templateParams, userID)
+      .then((response) => {
+        console.log('Email sent successfully!', response);
+        // You can show a success message or clear the form here
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+        // Handle the error, e.g., show an error message
+      });
+
+    // Clear the form inputs after submission
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
   return (
-    <div>Contact</div>
-  )
+    <div className="contact-form">
+      <h2>Contact Me</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Message</label>
+          <textarea
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+  
 }
 
 export default Contact
--- react-portfolio/src/components/resume/index.jsx
+++ react-portfolio/src/components/resume/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./resume.css"
 const Resume = () => {
   return (
-    <div>Resume</div>
+    
   )
 }
 

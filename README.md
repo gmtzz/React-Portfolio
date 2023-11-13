ef172950d4c4357cef529557ce68048f50a5b4e7
Giselle <martinezgiselle5@gmail.com>
2023-11-13 11:51:05 -0500

    added more projects

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # React-Portfolio
+* This portfolio is designed to emphasize React skills. It serves as a centralized hub to display your completed projects, highlight your skills, and provide contact information for potential collaboration.
+# Table of Contents
+* [Description](#Description)
+* [Installation](#Installation)
+* [DEMO-Recording](#DEMO-Recording)
+* [Screenshots](#Screenshots)
+* [Technologies-Used](#Technologies-used)
+* [Usage-information](#Usage-information)
+* [Credits](#Credits)
+* [Features](#Features)
+* [License](#License)
+# Description 
+* This app uses express js, mongoDB, mongoose and insomnia to manage a social network api platform.  
+
+# Installation
+Follow these to install and set up the app on your local machine.
+## Prerequisites
+    Please ensure you have the following before installation. 
+    - MongoDB
+    - Mongoose
+    - Express.js
+1. Clone the repo from https://github.com/gmtzz/Social-Network in your terminal
+ 
+2. Install dependencies using npm `npm install`
+
+3. Install and run MongoDB
+
+4. Configure environment variables in .env file 
+
+5. Run the app using commant `npm start`
+
+## Demo Recording
+* 
+## Screenshots
+ * NA
+## Technologies-Used
+* Express.JS
+* Mongoose
+* MongoDB
+* Insomnia
--- react-portfolio/src/components/projects/index.jsx
+++ react-portfolio/src/components/projects/index.jsx
@@ -2,30 +2,53 @@ import React from 'react'
 const projects = [
   {
     id: 1,
-    title: 'Project 1',
-    description: 'Description of Project 1',
-    githubLink: 'https://github.com/yourusername/project1',
-    deployedLink: 'https://yourproject1url.com',
-    imageSrc: 'project1-image.jpg',
+    title: 'Project 1: Fancy Minute Chef',
+    description: 'Fancy Minute Chef is food recipe search engine that searches the database using up to 5 keywords!',
+    githubLink: ' https://github.com/ejoseph10/Fancy-Minute-Chef',
+    deployedLink: ' https://ejoseph10.github.io/Fancy-Minute-Chef/',
+    imageSrc: './images/FancyMinuteChef.png',
   },
   {
     id: 2,
-    title: 'Project 2',
-    description: 'Description of Project 2',
-    githubLink: 'https://github.com/yourusername/project2',
-    deployedLink: 'https://yourproject2url.com',
-    imageSrc: 'project2-image.jpg',
+    title: 'Project 2: Roots Family Tree Application',
+    description: 'The Family Tree Application aims to enhance the user experience by providing a comprehensive platform for creating and visualizing family connections. Users can effortlessly manage their family tree, fostering a sense of connection and understanding of their familial relationships',
+    githubLink: 'https://github.com/eecmanny/Roots',
+    deployedLink: 'https://salty-thicket-14303-db98c73876c5.herokuapp.com/',
+    imageSrc: './images/project2.png',
   },
   {
     id: 3,
-    title: 'Project 3',
-    description: 'Description of Project 3',
-    githubLink: 'https://github.com/yourusername/project3',
-    deployedLink: 'https://yourproject3url.com',
-    imageSrc: 'project3-image.jpg',
+    title: 'Project 3: Flavr Recipe Blog',
+    description: ' **Flavr Plate**, a MERN (MongoDB, Express.js, React, Node.js) single-page application that brings at-home chefs together to share and discover recipes in a blog-style format. Connect with fellow food enthusiasts, explore diverse cuisines, and unleash your culinary creativity.',
+    githubLink: 'https://PENDING',
+    deployedLink: 'https://PENDING',
+    imageSrc: './images/flavrplate.png',
   },
-  //add 3 more cards with hw^^
-  // Add more projects with similar structure
+  {
+    id: 4,
+    title: 'Password Generator',
+    description: ' This password generator app uses HTML and CSS to randomly generate passwords with special characters, upper and lowercase characters, and a length between 8 and 128 characters. This app aids users in creating a password with complexities for emails, web accounts, etc.',
+    githubLink: 'https://github.com/gmtzz/Password-Generator',
+    deployedLink: 'https://gmtzz.github.io/Password-Generator/',
+    imageSrc: './images/passwordgenerator.png',
+  },
+  {
+    id: 5,
+    title: 'NoteTaker ',
+    description: ' The purpose of this app is to allow users to track an miscellaneous tasks or lists using the note taker application. This app utilizes express framework and node.js to allow developers to build the app using file and directory paths..',
+    githubLink: 'https://github.com/gmtzz/Note-Taker',
+    deployedLink: 'https://infinite-reef-00514-ec2bb9027c76.herokuapp.com/notes',
+    imageSrc: './images/notetaker.png',
+  },
+  {
+    id: 6,
+    title: 'Text Editor',
+    description: ' A browser-based text editor using idb package, javascript, and heroku app deployment technologies but also features offline functionality.',
+    githubLink: 'https://github.com/gmtzz/Text-Editor',
+    deployedLink: 'https://fathomless-brook-57267-efda70d657fc.herokuapp.com/',
+    imageSrc: './images/texteditor.png',
+  },
+  
 ];
 import "./projects.css"
 

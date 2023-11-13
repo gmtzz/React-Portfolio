08d6e38579517bdd32ddd55160a082acb2b4d730
Giselle <martinezgiselle5@gmail.com>
2023-11-13 09:08:24 -0500

    added resume to images folder

--- react-portfolio/src/components/resume/index.jsx
+++ react-portfolio/src/components/resume/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import "./resume.css"
+import resume from "../../images/GiselleMartinezResume.png"
 const Resume = () => {
+  const resumeURL = resume //add resume pdf to images folder
   return (
-    
+    <div>
+      <h2>My Resume</h2>
+      <p>Click the link below to download my resume:</p>
+      <a href={resumeURL} download="GiselleMartinezResume.png"> 
+        Download Resume
+      </a>
+      <iframe
+        src={resumeURL}
+        title="Resume"
+        width="100%"
+        height="500px"
+        style={{ border: '1px solid #ccc' }}
+      />
+    </div>
   )
 }
 
